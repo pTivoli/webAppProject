b6b3e73788ce8a97cd2f03891478405d43b1cb99
patrich <p.tivoli@hotmail.it>
2020-06-11 19:00:36 +0200

    new tables

--- DDL.sql
+++ DDL.sql
@@ -9,17 +9,25 @@ create table Persona(
 create table Medico(
   codiceRegionale varchar(10),
   medico_CFPersona char(16),
-  primary key(codiceRegionale, medico_Persona),
-  foreign key medico_Persona references Persona(codiceFiscale) on delete cascade
+  primary key(codiceRegionale, medico_CFPersona),
+  foreign key (medico_CFPersona) references Persona(codiceFiscale)
+  on delete cascade --da valutare (può essere anche un paziente il medico)
 );
 
 
-create table Persoale(
+create table Personale(
   mail varchar(30),
   pwd varchar(20),
   personale_CFPersona char(16),
-  primary key(personale_CFPersona),
-  foreign key personale_CFPersona references Persona(codiceFiscale) on delete cascade
+  ruolo VARCHAR(30),
+  primary key(personale_CFPersona, mail),
+  foreign key (personale_CFPersona) references Persona(codiceFiscale),
+  FOREIGN KEY (ruolo) REFERENCES Ruolo(nome)
+  on delete cascade --same thing of before
+);
+
+create table Ruolo(
+  nome VARCHAR(30) PRIMARY KEY
 )
 
 create table Ricetta(
@@ -29,5 +37,26 @@ create table Ricetta(
    ricetta_Codice_Medico varchar(10),
    ricetta_Medico_Persona char(16),
   primary key(codiceRicetta, ricetta_Codice_Medico, ricetta_Medico_Persona),
-  foreign key ricetta_Codice_Medico, ricetta_Medico_Persona references Medico(codiceRegionale, medico_Persona) on delete cascade
-)
+  foreign key (ricetta_Codice_Medico, ricetta_Medico_Persona) references Medico(codiceRegionale, medico_Persona)
+);
+
+create table Acquisto(
+  timest TIMESTAMP,
+  mailPersonale VARCHAR(30),
+  cfPersonale CHAR(16),
+  cfPersona CHAR(16),
+  totale INTEGER,
+  PRIMARY KEY (times, mailPersonale, cfPersonale),
+  FOREIGN KEY (cfPersona) REFERENCES Persona(codiceFiscale),
+  FOREIGN KEY (mailPersonale, cfPersonale) REFERENCES  Personale(mail, personale_CFPersona)
+);
+
+create table Farmacia(
+  nome VARCHAR(30),
+  indirizzo VARCHAR(30),
+  telefono VARCHAR(15),
+  cfPersonale CHAR(16),
+  mailPersonale VARCHAR(30),
+  PRIMARY KEY (nome, indirizzo, cfPersonale, mailPersonale),
+  FOREIGN KEY (cfPersonale, mailPersonale) REFERENCES Personale(personale_CFPersona, mail)
+);
