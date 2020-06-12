142433cb14d6297a474a03197e4e8d3a5a4480e9
Matteo <mmontinaro99@gmail.com>
2020-06-12 17:22:27 +0200

    deleted UserRegisterForm. Reg can activate pharmacies

--- src/REG.java
+++ src/REG.java
@@ -1,7 +1,36 @@
+import java.sql.Statement;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+import java.sql.Connection;
+
 public class REG {
 
-    public boolean activatePharmacy(Pharmacy pharmacy){
-        return false;
+    public boolean activatePharmacy(Pharmacy pharmacy) throws Exception{
+
+        Class.forName("");
+        Connection con = DriverManager.getConnection("localhost/PharmaPoint", "postgrsql", "Slashrocker1");
+
+        try {
+            try {
+                Statement st = con.createStatement();
+                int value = st.executeUpdate("INSERT INTO USER_DETAILS(NAME,ADDRESS,PHONENUMBER,PHARMACYDOCTOR) "
+                        + "VALUES('"
+                        + pharmacy.getName()
+                        + "','"
+                        + pharmacy.getAddress()
+                        + "','"
+                        + pharmacy.getPhoneNumber()
+                        + "','"
+                        + pharmacy.getPharmacyDoctor() + "')");
+            } catch (SQLException ex) {
+                return false;
+            }
+            con.close();
+        } catch (Exception e){
+            return false;
+        }
+
+        return true;
     }
 
     public boolean activatePM(PharmacistManager pharmacistManager){
