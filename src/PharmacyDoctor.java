a3f6835502f99533acae3e0b84a2144aa70307e0
patrich <p.tivoli@hotmail.it>
2020-06-12 18:07:18 +0200

    DeskOperator and PharmacistDoctor SQL Query added

--- src/PharmacyDoctor.java
+++ src/PharmacyDoctor.java
@@ -1,7 +1,7 @@
 import java.util.Date;
 import java.util.LinkedList;
 
-public class PharmacyDoctor {
+public class PharmacyDoctor extends DeskOperator{
 
     public boolean sellItemsWithReceipt(LinkedList<Medicine> medicineLinkedList, User user, Date date, Integer codRegDoc){
         return false;
