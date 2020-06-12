cd7b6700a84c87772466b7c60cf6c3f2fbb810e3
patrich <p.tivoli@hotmail.it>
2020-06-12 18:47:41 +0200

    registration of the user SQL completed

--- src/PharmacyDoctor.java
+++ src/PharmacyDoctor.java
@@ -1,5 +1,9 @@
 import java.util.Date;
 import java.util.LinkedList;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class PharmacyDoctor extends DeskOperator{
 
@@ -7,7 +11,20 @@ public class PharmacyDoctor extends DeskOperator{
         return false;
     }
 
-    public boolean registerUser(User user){
-        return false;
+    public void registerUser(User user) throws Exception {
+        try{
+            Class.forName("org.postgresql.Driver");
+            Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/PharmaPoint", "postgresql", "TivoliPatrick");
+            try{
+                Statement st = con.createStatement();
+                String queryUtente = "INSERT INTO Persona VALUES ('"+ user.getCF() +"',' "+ user.getfName() +"',' "+ user.getlName() +"','"+user.getDOB()+"')";
+                st.executeUpdate(queryUtente);
+            }catch(SQLException ex){
+                throw new Exception("Error DB");
+            }
+            con.close();
+        }catch(Exception e){
+            throw new Exception("Error DB");
+        }
     }
 }
