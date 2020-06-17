fdf41c1f63ecfd52d8d61f3a46dbec617c248293
patrich <p.tivoli@hotmail.it>
2020-06-17 22:26:19 +0200

    Session opened after login

--- src/it/pointPharma/DAOFormLogin/FormLoginDBAccess.java
+++ src/it/pointPharma/DAOFormLogin/FormLoginDBAccess.java
@@ -9,54 +9,63 @@ public class FormLoginDBAccess {
     private UserData userDataIn;
     private Pharmacist pharmacist;
     private String role;
+    private String cf;
 
-    public FormLoginDBAccess(UserData userData){
+    public FormLoginDBAccess(UserData userData) throws Exception {
         this.userDataIn = userData;
         this.pharmacist = null;
         this.role = null;
+        this.cf = null;
+        this.getUserData();
     }
 
-    public void getUserData() throws Exception {
+    private void getUserData() throws Exception {
         /*
             BE CAREFUL
             Check out if in your directory there is the postgres driver class.
             BE CAREFUL
             Change the credential in getConnection.
          */
-        try{
+        try {
             Connection con = DriverManager.getConnection("jdbc:postgresql://localhost/PharmaPoint", "postgres", "TivoliPatrick");
-            try{
+            try {
                 Statement st = con.createStatement();
-                String query = "SELECT mail, pwd, ruoloPersonale from Personale where mail='"+ userDataIn.getEmail() +"'"; //MAIL IS VALIDATED FROM HTML 5, SQL-INJECTION AVOIDED
+                String query = "SELECT * from Personale where mail='" + userDataIn.getEmail() + "'"; //MAIL IS VALIDATED FROM HTML 5, SQL-INJECTION AVOIDED
                 ResultSet ris = st.executeQuery(query);
                 String role = null;
+                String cf = null;
                 Pharmacist pharmacistRetrieved = new Pharmacist();
                 pharmacistRetrieved.setEmail(userDataIn.getEmail());
-                while(ris.next()) {
+                while (ris.next()) {
                     pharmacistRetrieved.setPwd(ris.getString("pwd"));
                     role = ris.getString("ruoloPersonale");
+                    cf = ris.getString("personale_cfpersona");
                 }
-                setParameters(pharmacistRetrieved, role);
-            }catch(SQLException e){
+                setParameters(pharmacistRetrieved, role, cf);
+            } catch (SQLException e) {
                 throw new Exception("Error DB");
             }
             con.close();
-        }catch(SQLException e){
+        } catch (SQLException e) {
             throw new Exception("Error DB");
         }
     }
 
-    private void setParameters(Pharmacist pharmacist, String ruolo){
+    private void setParameters(Pharmacist pharmacist, String ruolo, String cf) {
         this.pharmacist = pharmacist;
         this.role = ruolo;
+        this.cf = cf;
     }
 
-    public Pharmacist getPharmacist(){
+    public Pharmacist getPharmacist() {
         return this.pharmacist;
     }
 
-    public String getRole(){
+    public String getRole() {
         return this.role;
     }
 
+    public String getCF() {
+        return this.cf;
+    }
 }
--- src/it/pointPharma/beans/LoginFormAction.java
+++ src/it/pointPharma/beans/LoginFormAction.java
@@ -2,8 +2,7 @@ package it.pointPharma.beans;
 
 import it.pointPharma.DAOFormLogin.FormLoginDBAccess;
 import it.pointPharma.formBeans.UserData;
-import it.pointPharma.generalClasses.Pharmacist;
-import it.pointPharma.generalClasses.User;
+import it.pointPharma.generalClasses.*;
 import org.apache.struts.action.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,22 +15,41 @@ public class LoginFormAction extends Action {
         UserData loginData = (UserData) form;
         String password = loginData.getPassword();
         FormLoginDBAccess formLoginDBAccess = new FormLoginDBAccess(loginData);
-        formLoginDBAccess.getUserData();
-        Pharmacist pharmacist = formLoginDBAccess.getPharmacist();
+        String realPwd = formLoginDBAccess.getPharmacist().getPwd();
         String role = formLoginDBAccess.getRole();
         ActionForward af = null;
-        if(password.equals(pharmacist.getPwd())) {
+        if(password.equals(realPwd)) {
             switch (role) {
                 case "REG":
+                    REG reg = new REG();
+                    reg.setEmail(loginData.getEmail());
+                    reg.setCF(formLoginDBAccess.getCF());
+                    // other parameters are missed
+                    request.setAttribute("pharmacist", reg);
                     af = mapping.findForward("REG");
                     break;
                 case "PM":
+                    PharmacistManager pm = new PharmacistManager();
+                    pm.setEmail(loginData.getEmail());
+                    pm.setCF(formLoginDBAccess.getCF());
+                    // other parameters are missed
+                    request.setAttribute("pharmacist", pm);
                     af = mapping.findForward("PM");
                     break;
                 case "PD":
+                    PharmacyDoctor pd = new PharmacyDoctor();
+                    pd.setEmail(loginData.getEmail());
+                    pd.setCF(formLoginDBAccess.getCF());
+                    // other parameters are missed
+                    request.setAttribute("pharmacist", pd);
                     af = mapping.findForward("PD");
                     break;
                 case "DO":
+                    DeskOperator dop = new DeskOperator();
+                    dop.setEmail(loginData.getEmail());
+                    dop.setCF(formLoginDBAccess.getCF());
+                    // other parameters are missed
+                    request.setAttribute("pharmacist", dop);
                     af = mapping.findForward("DO");
                     break;
             }
