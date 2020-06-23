d909f7dbc971670fd53bcb6fa2fdbc1fb4c3f093
FraHackerino <20029793@studenti.uniupo.it>
2020-06-24 00:36:56 +0200

    query that decreases quantity of medicine in pharmacy warehouse created

--- src/it/pointPharma/generalClasses/DeskOperator.java
+++ src/it/pointPharma/generalClasses/DeskOperator.java
@@ -1,5 +1,6 @@
 package it.pointPharma.generalClasses;
 
+import javax.servlet.http.HttpSession;
 import java.sql.*;
 import java.util.LinkedList;
 
@@ -8,20 +9,30 @@ import java.util.Date;
 
 public class DeskOperator extends Pharmacist {
 
-    public void sellItems(LinkedList<Medicine> medicineLinkedList, Pharmacist pharmacist) throws Exception {
+    public void sellItems(LinkedList<Medicine> medicineLinkedList, Pharmacy pharmacy) throws Exception {
         try{
             Class.forName("org.postgresql.Driver");
-            Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/PharmaPoint", "postgresql", "TivoliPatrick");
+            Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/PharmaPoint", "postgresql", "Franci99");
             try {
                 Statement st = con.createStatement();
-                String queryVendita;
-                String queryVenditaFarmaco;
-                for (Medicine m: medicineLinkedList) {
-                    queryVendita = "INSERT INTO Acquisto VALUES ('" + getTime() + "','" + pharmacist.getCF() + "','" + pharmacist.getEmail() + "','" + this.getCF() + "','" + m.getCost() + "')";
-                    queryVenditaFarmaco = "INSERT INTO Acquisto VALUES ('" + "SELECT quantita FROM Magazzino_Farmaco" + "','" + getTime() + "','" + pharmacist.getCF() + "','" + m.getCode() + "')";
-                    st.executeUpdate(queryVendita);
-                    st.executeUpdate(queryVenditaFarmaco);
+                String queryPurchase;
+                String queryMedPurchase;
+                String mqty;
+                float totalCost = 0;
+                for (Medicine m  : medicineLinkedList ){
+                    totalCost += m.getCost();
+                    //queryMedPurchase = "INSERT INTO Acquisto_Farmaco VALUES()"
+
+                    mqty = ("update magazzino_farmaco as mf " +
+                            "set quantita = mf.quantita - 1 " +
+                            "where mf.codiceFarmaco = (select distinct farmaco.codice " +
+                            "from magazzino_farmaco " +
+                            "join farmacia on magazzino_farmaco.nomeFarmaciaMagazzino = farmacia.nome " +
+                            "join farmaco on magazzino_farmaco.codiceFarmaco = farmaco.codice " +
+                            "where magazzino_farmaco.codiceFarmaco = '" + m.getCode() + "' and farmacia.nome = '" + pharmacy.getName() + "'");
                 }
+
+
             } catch (SQLException e) {
                 throw new Exception("Error DB");
             }
