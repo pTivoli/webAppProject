49d17dda3b17e216e5443bf775c6b07ae63e638c
Matteo <mmontinaro99@gmail.com>
2020-06-25 00:43:54 +0200

    eliminated useless imports

--- src/it/pointPharma/generalClasses/PharmacyDoctor.java
+++ src/it/pointPharma/generalClasses/PharmacyDoctor.java
@@ -1,8 +1,6 @@
 package it.pointPharma.generalClasses;
 
 import java.sql.*;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.LinkedList;
 
